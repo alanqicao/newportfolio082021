6efa1c2517cc60dc82aadc1c81490ad9b5c3f9c8
Qi Cao <alanqicao@gmail.com>
2021-08-08 18:20:07 -0600

    Intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, { 
+      showCursor: false, 
+      backDelay: 1500,
+      backSpeed: 60,
+      showCursor: true,
+      strings: ['Software Developer', 'Violin Maker' ], });
+    
+  }, [])
+
   return (
     <div className="intro" id="intro">
-      intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Alan Cao</h1>
+          <h3>Professions <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio"> 
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
